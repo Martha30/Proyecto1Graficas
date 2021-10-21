843686dd33c715ede9cb1b666c72acc5d9d945eb
Martha30 <53914325+Martha30@users.noreply.github.com>
2021-10-20 20:27:52 -0600

    cambios, incluyendo nuevos modelos

--- obj.py
+++ obj.py
@@ -1,28 +1,103 @@
+#Laurelinda Gómez
+#19501
+
+import struct
+def color(r, g, b):
+    return bytes([ int(b * 255), int(g* 255), int(r* 255)])
+
+#En relación a lo que vimos en la clase
 class Obj(object):
     def __init__(self, filename):
         with open(filename) as f:
-            self.lines = f.read().splitlines()
-        
-        self.vertices = []
-        self.texcoords = []
-        self.normals = []
+            self.lineas = f.read().splitlines()
+        self.verts = []
+        self.tverts = []
+        self.normales = []
         self.faces = []
         self.read()
-          
+
+    def read(self):
+        m1 = 0
+        m2 = 0
+        m3 = 0
+        for linea in self.lineas:
+            if linea:
+                try:
+                    prefix, val = linea.split(' ', 1)
+                except:
+                    prefix =''
+                if prefix =='v':
+                    vert = list(map(float, val.split(' ')))
+                    self.verts.append(vert)
+                    if vert[0]>m2:
+                        m2 = vert[0]
+                    if vert[1]>m1:
+                        m1 = vert[1]
+                    if vert[2]>m3:
+                        m3 = vert[2]
+                elif prefix =='vt':
+                    self.tverts.append(
+                        list(map(float, val.split(' ')))
+                    )
+                elif prefix == 'vn':
+                    self.normales.append(
+                        list(map(float, val.split(' ')))
+                  )
+                elif prefix == 'f':
+                    faces = val.split(' ')
+                    if(len(faces)==3):
+
+                        self.faces.append(
+                            [list(map(int, face.split('/'))) for face in faces if len(face)>2]
+                        )
+                    else:
+                        faces1 = [faces[0], faces[1], faces[2]]
+                        faces2 = [faces[0], faces[2], faces[3]]
+
+                        self.faces.append(
+                            [list(map(int, face.split('/'))) for face in faces1 if len(face)>2]
+                        )
+
+                        self.faces.append(
+                            [list(map(int, face.split('/'))) for face in faces2 if len(face)>2]
+                        )
+
+        self.vertexN = [[i[0]/m2, i[1]/m1, i[2]/m3]for i in self.verts]
+        self.verts = self.vertexN
+
+class Texture(object):
+    def __init__(self, path):
+        self.path = path
+        self.read()
     
     def read(self):
-        for line in self.lines:
-            if line: 
-                prefix, value  = line.split(' ', 1)
-                if prefix == 'v':
-                    self.vertices.append(list(map(float, value.split(' '))))
-                #Texture Coordinates
-                elif prefix == 'vt': 
-                    self.texcoords.append(list(map(float, value.split(' '))))
-                #Normales
-                elif prefix == 'vn': 
-                    self.normals.append(list(map(float, value.split(' '))))
-                elif prefix == 'f':
-                    self.faces.append([list(map(int, face.split('/'))) for face in value.split(' ')])            
-    
+        image = open(self.path, 'rb')
+        image.seek(2 + 4 + 4)
+        #header
+        headerSize = struct.unpack('=l', image.read(4))[0]
 
+        image.seek(2 + 4 + 4 + 4 + 4)
+        #el ancho
+        self.width = struct.unpack('=l', image.read(4))[0]
+        #altura
+        self.height = struct.unpack('=l', image.read(4))[0]
+        image.seek(headerSize)
+        self.pixels = []
+        
+        for y in range(self.height):
+            self.pixels.append([])
+            for x in range(self.width):
+                b = ord(image.read(1))
+                g = ord(image.read(1))
+                r = ord(image.read(1))
+                self.pixels[y].append(color(r, g, b))
+        image.close()
+        
+    def get_color(self, tx, ty):
+        x = int(tx * self.width)
+        y = int(ty * self.height)
+        try:
+            return self.pixels[y][x]
+        except:
+            return color(0, 0, 0)
+        
---
+++ shaders.py
@@ -0,0 +1,111 @@
+#Laurelinda Gómez
+#19501
+
+#main
+from gl3 import *
+
+pi = 3.14
+
+def gourad(render, **kwargs):
+    #barycentric
+    w, v, u = kwargs['bar']
+    #textura
+    tx, ty = kwargs['texture_coords']
+    #normals
+    nA, nB, nC = kwargs['varying_normals']
+    diver = render.current_texture.get_color(tx, ty)
+     # intensidad de luz
+    iA, iB, iC = [dot(n, render.light) for n in (nA, nB, nC)]
+    intensity = w*iA + v*iB + u*iC
+    return diver * intensity
+
+
+def shader1(render, **kwargs):
+    #mesa
+    w, v, u = kwargs['bar']
+    nA, nB, nC = kwargs['varying_normals']
+    A, B, C = kwargs['triangle']
+    diver = color(241, 180, 109)
+    iA, iB, iC = [dot(n, render.light) for n in (nA, nB, nC)]
+    intensity = w*iA + v*iB + u*iC
+    return diver * intensity
+
+def shader2(render, **kwargs):
+    # lata
+    w, v, u = kwargs['bar']
+    nA, nB, nC = kwargs['varying_normals']
+    A, B, C = kwargs['triangle']
+    diver = color(249, 219, 227)
+    iA, iB, iC = [dot(n, render.light) for n in (nA, nB, nC)]
+    intensity = w*iA + v*iB + u*iC
+    return diver * intensity
+
+def shader3(render, **kwargs):
+    #Lentes
+    w, v, u = kwargs['bar']
+    nA, nB, nC = kwargs['varying_normals']
+    A, B, C = kwargs['triangle']
+    diver = color(242, 208, 102)
+    iA, iB, iC = [dot(n, render.light) for n in (nA, nB, nC)]
+    intensity = w*iA + v*iB + u*iC
+    return diver * intensity
+
+def shader4(render, **kwargs):
+    #succulent
+    w, v, u = kwargs['bar']
+    nA, nB, nC = kwargs['varying_normals']
+    A, B, C = kwargs['triangle']
+    diver = color(0,0,255)
+    iA, iB, iC = [dot(n, render.light) for n in (nA, nB, nC)]
+    intensity = w*iA + v*iB + u*iC
+    return diver * intensity
+
+def shader5(render, **kwargs):
+    #quran
+    w, v, u = kwargs['bar']
+    nA, nB, nC = kwargs['varying_normals']
+    A, B, C = kwargs['triangle']
+    diver = color(255,255,0)
+    iA, iB, iC = [dot(n, render.light) for n in (nA, nB, nC)]
+    intensity = w*iA + v*iB + u*iC
+    return diver * intensity
+
+
+r = Renderer(900,900)
+r.lookAt(V3(0,0,5), V3(0, 0, 0), V3(0, 1, 0))
+r.current_texture = gourad
+r.light = V3(-0.3, -0.3, 0.4)
+#Mesa 
+r.load('./models/table.obj', (-0.3, -0.6, 0), (0.8, 0.6, 0.1), (0, pi/3, 0))
+r.active_shader = shader1
+r.draw_arrays('TRIANGLES')
+
+r.current_texture = gourad
+r.light = V3(-0.3, -0.3, 0.4)
+#Lentes
+r.load('./models/Glasses.obj', (-0.3, 0.2, 0), (0.05, 0.2, 0.05), (0, pi/3, 0))
+r.active_shader = shader2
+r.draw_arrays('TRIANGLES')
+
+r.current_texture = gourad
+r.light = V3(-0.3, -0.3, 0.4)
+#Lata 
+r.load('./models/Can.obj', (-0.6, 0.2, 0), (0.08, 0.2, 0.05), (0, pi/3, 0))
+r.active_shader = shader3
+r.draw_arrays('TRIANGLES')
+
+r.current_texture = gourad
+r.light = V3(-0.3, -0.3, 0.4)
+#plantita
+r.load('./models/SUCCULENT.obj', (0.7, -0.4, 0), (0.25, 0.6, 0.1), (0, -pi/4, 0))
+r.active_shader = shader4
+r.draw_arrays('TRIANGLES')
+
+r.current_texture = gourad
+r.light = V3(-0.3, -0.3, 0.4)
+#Libro
+r.load('./models/Quran.obj', (0.5, 0.2, 0), (0.05, 0.2, 0.05), (0, pi/3, 0))
+r.active_shader = shader5
+r.draw_arrays('TRIANGLES')
+r.render()
+
